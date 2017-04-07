cba7e1e3afbe072fa82f2f9d6b5a1e34c11d81fd
Yuki Ishikawa <mazeltov17@gmail.com>
2017-04-07 16:17:50 +0900

    add devise, generate model user (before rails g devise user)

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407070207) do
+ActiveRecord::Schema.define(version: 20170407071652) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
